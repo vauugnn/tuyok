ad01443c831c24cc0e3d2d32cd502e503e53aa47
Vaughn Roche de Roda <vaughnrochederoda@gmail.com>
2024-01-31 14:32:44 +0800

    discount button is working

--- app.py
+++ app.py
@@ -4,15 +4,29 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def calculate_fare():
+    fare = None  # Initialize fare for initial page load
+
     if request.method == "POST":
-        distance = request.form["enter_destination"]
-        distance2 = int(distance)
-        if distance2 <= 4:
-            fare = 15
+        distance = int(request.form.get('enter_destination'))
+        apply_discount = request.form['discount'] == 'on'
+
+        if apply_discount:
+            fare = calculate_fare_with_discount(distance)
         else:
-            fare = 15 + (distance2 - 4) * 2
-        return render_template("index.html", fare=fare)
-    return render_template("index.html")
+            fare = calculate_fare_regular(distance)
+
+    return render_template("index.html", fare=fare)
+
+def calculate_fare_regular(distance):
+    if distance <= 4:
+        return 15
+    else:
+        return 15 + (distance - 4) * 2
+
+def calculate_fare_with_discount(distance):
+    regular_fare = calculate_fare_regular(distance)
+    return int((regular_fare * 0.8))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
