d5890a1fe4a8883eea047d811628b3b6bf40fb50
Vaughn Roche de Roda <vaughnrochederoda@gmail.com>
2024-02-05 03:06:51 +0800

    map functionality is working, will deploy prototype

--- app.py
+++ app.py
@@ -4,42 +4,57 @@ from geopy.geocoders import Nominatim
 
 app = Flask(__name__)
 
-from geopy.geocoders import Nominatim
-
 @app.route("/", methods=["GET", "POST"])
 def calculate_fare():
     fare = None  # Initialize fare for initial page load
 
     # Set default coordinates for Davao City
-    geolocator = Nominatim(user_agent="myGeocoder")
+    geolocator = Nominatim(user_agent="FareCalculator (myemail@example.com)")
     location = geolocator.geocode("Davao City")
     coords = [location.latitude, location.longitude]
 
+    # Get the coordinates of the two points
+    point_a = (51.7519, -1.2578)
+    point_b = (50.8429, -0.1313)
+
+    # Calculate the distance between the two points in kilometers
+    distance = geodesic(point_a, point_b).kilometers
+
     if request.method == "POST":
-        distance = int(request.form.get('enter_destination'))
         apply_discount = request.form.get('discount') == 'on'
 
-        if apply_discount:
+        if distance and apply_discount:
             fare = calculate_fare_with_discount(distance)
-        else:
+        elif distance:
             fare = calculate_fare_regular(distance)
+    return render_template("index.html", fare=fare, coords=coords, distance=distance, point_a=point_a, point_b=point_b)
 
-    return render_template("index.html", fare=fare, coords=coords)
 
 @app.route('/distance', methods=['GET'])
 def get_distance():
     point_a = (51.7519, -1.2578)
     point_b = (50.8429, -0.1313)
     distance = geodesic(point_a, point_b).miles
+    print(distance)
     return jsonify({'distance': distance})
 
 @app.route('/map', methods=['GET'])
 def map():
-    geolocator = Nominatim(user_agent="myGeocoder")
+    geolocator = Nominatim(user_agent="FareCalculator (vaughnrochederoda@gmail.com)")
     location = geolocator.geocode("Davao City")
     return render_template("index.html", coords=[location.latitude, location.longitude])
-    # except Exception as e:
-    #     print(e)
+
+@app.route('/calculate_fare', methods=['POST'])
+def calculate_fare_post():
+    distance = float(request.form.get('distance'))  # Get the distance from the form
+    apply_discount = request.form.get('discount') == 'on'
+
+    if apply_discount:
+        fare = calculate_fare_with_discount(distance)
+    else:
+        fare = calculate_fare_regular(distance)
+
+    return jsonify({'fare': 'Your fare is: ₱' + str(fare)})
 
 def calculate_fare_regular(distance):
     if distance <= 4:
